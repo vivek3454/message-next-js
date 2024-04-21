a28c25d6fed8fc94f842ffc6a78f40b293eb667d
dev03lkomavyax <149859737+dev03lkomavyax@users.noreply.github.com>
2024-04-21 14:32:51 +0530

    code updated

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,11 @@
+import "next-auth"
+
+
+declare module "next-auth" {
+    interface User{
+        _id?: string;
+        isVerified?: boolean;
+        isAcceptingMessage?: boolean;
+        username?:string;
+    }
+}
